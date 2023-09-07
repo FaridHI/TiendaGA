a01f09a0829ebb018c578718f69b70fdba7a016a
FaridHI <1121041@senati.pe>
2023-09-07 11:42:33 -0500

    cree un nuevo forms (ventana de registro)

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(169, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 30);
+            label1.TabIndex = 0;
+            label1.Text = "REGISTRO";
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(470, 368);
+            Controls.Add(label1);
             Name = "Form2";
             Text = "Form2";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
