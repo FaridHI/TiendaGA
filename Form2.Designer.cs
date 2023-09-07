7da411beaf3a22b6f9ad92f57a0bb92f492f300c
FaridHI <1121041@senati.pe>
2023-09-07 12:04:00 -0500

    agregue logo de registro

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,27 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Black", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(169, 19);
+            label1.Location = new Point(185, 20);
             label1.Name = "label1";
             label1.Size = new Size(116, 30);
             label1.TabIndex = 0;
             label1.Text = "REGISTRO";
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._6073873;
+            pictureBox1.Location = new Point(269, 67);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(208, 194);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(38, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(38, 129);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Conraseña:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(38, 195);
+            label4.Name = "label4";
+            label4.Size = new Size(179, 21);
+            label4.TabIndex = 4;
+            label4.Text = "Confirmar contraseña:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.InactiveCaption;
+            textBox1.Location = new Point(38, 91);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(177, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.InactiveCaption;
+            textBox2.Location = new Point(38, 153);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(177, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.InactiveCaption;
+            textBox3.Location = new Point(38, 219);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(177, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Black", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(269, 314);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 34);
+            button1.TabIndex = 8;
+            button1.Text = "&Registrarse";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Black", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(109, 314);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 34);
+            button2.TabIndex = 9;
+            button2.Text = "&Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(470, 368);
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(516, 399);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "Form2";
-            Text = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Registro";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +166,14 @@
         #endregion
 
         private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
     }
 }
